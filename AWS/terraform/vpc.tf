7099a0506054ebc0030e9e529ded5438420df202
zeze1004 <thwjd2717@naver.com>
2022-01-03 20:35:59 +0900

    subnet 설정 추가

--- AWS/terraform/vpc.tf
+++ AWS/terraform/vpc.tf
@@ -1,3 +1,4 @@
+# VPC 설정
 provisioner "aws" {
 	region = "ap-norheast-2"
 }
@@ -8,4 +9,26 @@ resource "aws_vpc" "main" {
 	tags = {
 		Name = "vpc_terraform"
 	}
+}
+
+#  Subnet 설정
+resource "aws_subnet" "public_subnet" {
+	# 위에서 지정한 vpc 지정
+	vpc_id = aws_vpc.main.id
+	cidr_block = "10.0.0.0/24"
+
+	availability_zone = "ap-norheast-2a"
+
+	tags = {
+	  Name = "public-subnet"
+	}
+}
+resource "aws_subnet" "private_subnet" {
+	vpc_id = aws_vpc.main.id
+	cidr_block = "10.0.10.0/24"
+
+	tags = {
+	  Name = "private-subnet"
+	}
+  
 }
